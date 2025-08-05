d79d8a763fe4d5eb6400b8183765fcbfb93225b8
Mudz <mudz@Mudzs-Mac-mini.local>
2025-08-05 16:51:29 +0300

    new update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 4000;
 const repliApp = process.env.APP_NAME
 
 app.use('/nginx_file', express.static(path.join(__dirname, 'nginx_file')));
-
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'nginx_page.html'));
     console.log(`Request served by ${repliApp}`);
